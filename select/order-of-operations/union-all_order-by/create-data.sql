a7729accbeb387a786bda19666e3a054c13d0293
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-11 17:20:21 +0200

    remove selects

--- select/order-of-operations/union-all_order-by/create-data.sql
+++ select/order-of-operations/union-all_order-by/create-data.sql
@@ -8,11 +8,3 @@ insert into tq84_tab_1 values (5, 'five' );
 insert into tq84_tab_2 values (6, 'six'  );
 insert into tq84_tab_2 values (7, 'seven');
 insert into tq84_tab_2 values (3, 'three');
-
-select * from tq84_tab_1 union all
-select * from tq84_tab_2
-order by
-  id;
-
-(select * from tq84_tab_1 order by id) union all
-(select * from tq84_tab_2 order by id)
