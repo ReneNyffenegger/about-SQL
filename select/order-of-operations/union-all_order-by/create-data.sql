01224652cd91be70d459ba0f2b8e9ce2fb61787e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-08-25 17:01:23 +0200

    Put insert statements in begin/end block

--- select/order-of-operations/union-all_order-by/create-data.sql
+++ select/order-of-operations/union-all_order-by/create-data.sql
@@ -1,10 +1,13 @@
 create table tq84_tab_1 (id integer, txt varchar(10));
 create table tq84_tab_2 (id integer, txt varchar(10));
 
-insert into tq84_tab_1 values (4, 'four' );
-insert into tq84_tab_1 values (1, 'one'  );
-insert into tq84_tab_1 values (5, 'five' );
+begin
+   insert into tq84_tab_1 values (4, 'four' );
+   insert into tq84_tab_1 values (1, 'one'  );
+   insert into tq84_tab_1 values (5, 'five' );
 
-insert into tq84_tab_2 values (6, 'six'  );
-insert into tq84_tab_2 values (7, 'seven');
-insert into tq84_tab_2 values (3, 'three');
+   insert into tq84_tab_2 values (6, 'six'  );
+   insert into tq84_tab_2 values (7, 'seven');
+   insert into tq84_tab_2 values (3, 'three');
+end;
+/
